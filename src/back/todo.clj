91b5b9973da2d3e1c3ad139864bfa4b65f789ff9
raoul2000 <manu34@free.fr>
2022-08-24 23:45:12 +0200

    back: /todo change int keys to string

--- src/back/todo.clj
+++ src/back/todo.clj
@@ -1,9 +1,9 @@
 (ns todo
   (:require [response :as resp]))
 
-(def todo-list (atom {1   {:text  "do something funny"
+(def todo-list (atom {"1"   {:text  "do something funny"
                            :done  false}
-                      2   {:text  "do something clever"
+                      "2"   {:text  "do something clever"
                            :done  true}}))
 
 (defn read 
