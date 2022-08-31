96ef1b331b86fcd6453cd648d59ab3b29c11806a
raoul2000 <manu34@free.fr>
2022-09-01 00:32:08 +0200

    common:  spec and read/write utils for todo data

--- src/shared/todo/db.cljc
+++ src/shared/todo/db.cljc
@@ -9,16 +9,54 @@
   {:todo/text  text
    :todo/done  done})
 
+(comment
+  
+  (create-todo-item "e" true)
+  )
+
 (defn create-todo-list [] {})
+(defn create-ordered-ids [] [])
 
+(defn create
+  "Create an returns an empty DB to manage todo list"
+  []
+  {:post [(s/valid? :todo/db %)]}
+  {:todo-list (create-todo-list)
+   :ordered-ids (create-ordered-ids)})
 
-(defn add-item 
+(defn new-id []
+  (str (random-uuid)))
+
+(defn add-item-to-list
   "Assoc *new-item* to key *new-id* in the given todo list. When *new-id* is already
-   present in the todo list, its value is updated with *new-item*" 
+   present in the todo list, its value is updated with *new-item*"
   [todo-list new-id new-item]
   {:pre [(s/valid? :todo/todo-list todo-list)
-         (s/valid? :todo/id  new-id)
-         (s/valid? :todo/item  new-item)]
+         (s/valid? :todo/id        new-id)
+         (s/valid? :todo/item      new-item)]
    :post [(s/valid? :todo/todo-list %)]}
   (assoc todo-list new-id new-item))
+
+(comment
+  
+(add-item-to-list {} "E" (create-todo-item "e" true))
+
+  )
+(defn delete-item-from-list [todo-list item-id]
+  {:pre [(s/valid? :todo/todo-list todo-list)
+         (s/valid? :todo/id  item-id)]
+   :post [(s/valid? :todo/todo-list %)]}
+  (dissoc todo-list item-id))
+
+;;(defn update-list-item [todo-list item-id updated-item])
+
+(defn add-item [db item-id item]
+    {:pre [(s/valid? :todo/todo-list db)
+           (s/valid? :todo/id  item-id)]
+     :post [(s/valid? :todo/todo-list %)]}
+  (-> db
+      (update :todo-list add-item-to-list item-id item)
+      (update :ordered-ids conj item-id)))
+
+
 
--- test/shared/todo/db_test.clj
+++ test/shared/todo/db_test.clj
@@ -12,21 +12,50 @@
     (is (= {}
            (db/create-todo-list)))))
 
+(deftest create-test
+  (testing "creating an empty db"
+    (is (= {:todo-list {}
+            :ordered-ids []}
+           (db/create)))))
 
-(deftest add-item-test
+(deftest add-item-to-list-test
 
   (testing "adding todo item to empty todo list"
     (is (= {"id1" #:todo{:text "description", :done false}}
-           (db/add-item {} "id1" (db/create-todo-item "description" false)))))
+           (db/add-item-to-list {} "id1" (db/create-todo-item "description" false)))))
 
   (testing "adding todo item to non empty todo list"
-    (let [todo-list {"id1" #:todo{:text "description", :done false}}]
-      (is (= {"id1" #:todo{:text "description", :done false},
-              "id2" #:todo{:text "another text", :done true}}
-             (db/add-item todo-list "id2" (db/create-todo-item "another text" true))))))
-  
-  (testing "adding todo item for existing id in the todo list"
-    (let [todo-list {"id1" #:todo{:text "description", :done false}}]
-      (is (= {"id1" #:todo{:text "another text", :done true}}
-             (db/add-item todo-list "id1" (db/create-todo-item "another text" true))))))
-  )
+      (is (= {"id1" #:todo{:text "description", 
+                           :done false},
+              "id2" #:todo{:text "another text", 
+                           :done true}}
+             (db/add-item-to-list {"id1" #:todo{:text "description", 
+                                                :done false}}
+                                  "id2" 
+                                  (db/create-todo-item "another text" true)))))
+
+  (testing "adding todo item for existing id in the todo list" 
+      (is (= {"id1" #:todo{:text "another text"
+                           :done true}}
+             
+             (db/add-item-to-list {"id1" #:todo{:text "description"
+                                                :done false}}
+                                  "id1" 
+                                  (db/create-todo-item "another text" true))))))
+
+(deftest delete-item-from-list-test
+  (testing "removing item from todo list"
+    (is (= {"id2" #:todo{:text "another text", :done true}}
+           (db/delete-item-from-list {"id1" #:todo{:text "description", :done false},
+                                      "id2" #:todo{:text "another text", :done true}}
+                                     "id1"))))
+  (testing "removing last item returns an empty todo list"
+    (is (empty? (db/delete-item-from-list {"id2" #:todo{:text "another text", :done true}}
+                                          "id2"))))
+
+  (testing "removing item not in list returns same list"
+    (is (= {"id2" #:todo{:text "another text", :done true}}
+           (db/delete-item-from-list {"id2" #:todo{:text "another text", :done true}} "id1"))))
+
+  (testing "removing item from an empty list returns empty list"
+    (is (empty? (db/delete-item-from-list {} "id")))))
